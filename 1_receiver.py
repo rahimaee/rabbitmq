41abf59d20bee31c4fde3ae3fb7397e746093e92
moh <moh@kali.kali>
2023-11-22 23:43:02 +0330

    learn act

--- 1_receiver.py
+++ 1_receiver.py
@@ -1,11 +1,22 @@
 import pika
-
+import time
 credeintals =  pika.PlainCredentials('mohammad','mohammad')
 conntions =  pika.BlockingConnection(pika.ConnectionParameters(host='localhost',credentials=credeintals))
 ch = conntions.channel()
 ch.queue_declare(queue='one')
 
-def callback(ch,method,prop,body):
+ch.basic_qos(prefetch_count=1)
+
+def callback(ch,method,properties,body):
     print(f'received {body}')
-ch.basic_consume('one',on_message_callback=callback,auto_ack=True)
+    print(f'prop {properties}')
+    print(method)
+    time.sleep(3)
+    print("done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+
+ch.basic_consume('one',on_message_callback=callback)
+
 ch.start_consuming()
--- 1_sender.py
+++ 1_sender.py
@@ -6,6 +6,6 @@ connctions = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
 
 ch =  connctions.channel()
 ch.queue_declare(queue='one')
-ch.basic_publish(exchange='',routing_key='one',body='hello word 1',properties=pika.BasicProperties(content_type='test/plain',content_encoding='zgip',user_id='10',delivery_mode=2,))
+ch.basic_publish(exchange='',routing_key='one',body='hello word 1',properties=pika.BasicProperties(content_type='test/plain',delivery_mode=2,))
 print('msg send it...')
 connctions.close()
